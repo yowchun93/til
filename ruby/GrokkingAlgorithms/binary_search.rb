775c30b115c24f447f8949f489804ab8a38e3173
yowchun <yowchun93@gmail.com>
2018-05-21 11:17:29 +0800

    implementation of binary search in ruby

---
+++ ruby/GrokkingAlgorithms/binary_search.rb
@@ -0,0 +1,18 @@
+def binary_search(list,item) 
+    low = 0
+    high = list.length - 1
+    while low <= high 
+      mid = (low + high) / 2
+      guess = list[mid]
+      # if guss is too high , 
+      if guess == item
+        return guess
+      elsif guess < item
+        low = guess + 1
+      else
+        high = guess - 1
+      end
+    end
+end
+
+puts binary_search((1..1000000).to_a, 55)
