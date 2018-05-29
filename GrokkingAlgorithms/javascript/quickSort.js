a061bb6902920b403bbd3be38709e300d410e194
yowchun <yowchun93@gmail.com>
2018-05-29 16:31:21 +0800

    added breadthFirstSearch implementation is JS

--- GrokkingAlgorithms/javascript/quickSort.js
+++ GrokkingAlgorithms/javascript/quickSort.js
@@ -5,35 +5,54 @@
 // and larger than pivot
 // 3. after partitioning, perform the same on the array on the 
 // left and on the right 
+
 function quickSort(array) {
-  var sortedArray = []
-  if(array.length < 2 ) {
-    return array
+  if (array.length < 2 ) {
+    return array;
   }
-  else {
-    pivot = array[array.length - 1];
-    less = partition(array, pivot).left;
-    more = partition(array, pivot).right;
-    return sortedArray.concat(quickSort(less), pivot , quickSort(more));
-  }
+  pivot = array[array.length - 1];
+  less = partition(array, pivot).left;
+  more = partition(array, pivot).right;
+  debugger
+  return quickSort(less).concat(pivot, quickSort(more));
+  // return sortedArray.concat(quickSort(less), pivot , quickSort(more));
 }
 
-// method to partition array, according to 
+// // method to partition array, according to 
 function partition(array, pivot) {
   leftArray = []
   rightArray = []
-  array.forEach(function(element) {
-    if(element < pivot) {
-      leftArray.push(element)
+  for(var i = 1; i < array.length; i++) {
+    if(array[i] < pivot) {
+      leftArray.push(array[i]);
     } else {
-      rightArray.push(element)
+      rightArray.push(array[i]);
     }
-  })
+  }
   return {left: leftArray,right: rightArray};
 }
 
-console.log(partition([4,5,3,2,7],6))
-console.log(quickSort([4,5,3,2,7,6]))
 
+function quickSortBasic(array) {
+  if (array.length < 2) {
+    return array;
+  }
+  var pivot = array[0]
+  var lesser = []
+  var greater = []
+
+  for(var i = 1; i < array.length; i++) {
+    if(array[i] < pivot) {
+      lesser.push(array[i]);
+    } else {
+      greater.push(array[i]);
+    }
+  }
+  return quickSortBasic(lesser).concat(pivot, quickSortBasic(greater));
+}
+
+// console.log(partition([4,5,3,2,7],6))
+console.log(quickSort([4,5,3,2,7,6]))
+console.log(quickSortBasic([4,5,3,2,7,6]))
 
 
