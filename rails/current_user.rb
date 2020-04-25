1acd318e7fcb3d998c1505e6448ef1b1c32197e2
yc yap <yowchun93@gmail.com>
2020-04-25 17:52:19 +0800

    add comment

--- rails/current_user.rb
+++ rails/current_user.rb
@@ -1,4 +1,7 @@
 
+## Credit to: Ryan Bigg's blogpos
+## https://ryanbigg.com/2017/06/current-considered-harmful
+
 ## sometimes, you need to access current_user in places outside of controller,
 ## this would be a convenient way to access current_user
 def current_user
